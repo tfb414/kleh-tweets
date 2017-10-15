4156b590108990ce3d97cd43271d589003a96b67
Tim Brady <tfb414@gmail.com>
2017-10-15 16:32:22 -0400

    trying to connect to reddit api

--- main.js
+++ main.js
@@ -1,8 +1,20 @@
 const twitter = require('./twitter.js')
 
 function main() {
-  twitter.init();
-  console.log('main')
+  let latestTweet;
+  setInterval(() => {
+    twitter.checkTweets().then((tweet) => {
+
+    })
+  }, 1000);
+
+  //logic for is it the same tweet
+  //post to reddit
+  //deal with potential errors
+
+
+
+
 }
 
 main();
--- twitter.js
+++ twitter.js
@@ -1,31 +1,33 @@
 const Twitter = require('twitter');
+const env = require('dotenv').config();
 
-function init() {
+function checkTweets() {
     console.log('init ran')
 
     var client = new Twitter({
         consumer_key: process.env.TWITTER_CONSUMER_KEY,
         consumer_secret: process.env.TWITTER_CONSUMER_SECRET,
-        access_token_key: process.env.
-        // bearer_token: process.env.TWITTER_BEARER_TOKEN
+        bearer_token: process.env.TWITTER_BEARER_TOKEN
     });
+
+
 
     // client.get('search/tweets', { q: 'node.js' }, function (error, tweets, response) {
     //     console.log(tweets);
     // });
 
-    client.post('statuses/update', { status: 'I Love Twitter' })
-        .then(function (tweet) {
-            console.log(tweet);
-        })
-        .catch(function (error) {
-            throw error;
-        })
+    return client.get('statuses/user_timeline', { screen_name: 'rustupdates' })
+        .then((tweets) => {
+            return tweets[0]['text'];
+        }).catch(console.log)
+
+
+
 }
 
 
 
 
 module.exports = {
-    init: init
+    checkTweets: checkTweets
 }
